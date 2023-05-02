0fe7a1231fd0447e7183fe393a80266c3c2fb1e8
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-05-02 18:16:42 -0400

    higher order native array

--- src/04-find.js
+++ src/04-find.js
@@ -13,21 +13,30 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findPinkElephantsByTimestreet(songs) {}
+function findPinkElephantsByTimestreet(songs) {
+  let findByTitle = songs.find(song => song.title === "Pink Elephants");
+  return findByTitle; 
+}
 
 /**
  * Returns the first song in the list that is under three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+function findFirstSongUnderThreeMinutes(songs) {
+  let findFirstSong = songs.find(song => song.runtimeInSeconds < 180);
+  return findFirstSong;
+}
 
 /**
  * Returns the first song in the list where the song title equals the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstTitleTrack(songs) {}
+function findFirstTitleTrack(songs) {
+  let findBySameName = songs.find(song => song.title === song.album);
+  return findBySameName; 
+}
 
 module.exports = {
   findPinkElephantsByTimestreet,
